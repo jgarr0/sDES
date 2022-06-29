88d03dee2073c930e797c8f2a0f08bad55e6ba75
Savannah Rimmele <rimmeles@gmail.com>
2022-06-29 13:41:07 -0400

    Deriving subkeys: Working
    Begin Initial and Final Permutations
    
    Co-authored-by: Joseph Garro <jgarr0@users.noreply.github.com>

--- sDES.py
+++ sDES.py
@@ -61,36 +61,55 @@ class sDES:
         temp_input = temp_input | addme
         return temp_input
 
+    def permutate(self, input, dictionary, length=10):
+        temp = ""
+        input_copy = list(bin(input)[2:].zfill(length))
+        for i in range(0, len(dictionary)):
+            temp += input_copy[dictionary[i]-1]
+        return "".join(temp)
+
     # function definitions
     def deriveKeys(self):                   # 2^10
         # form P10
-        P10_LST = list(bin(0x3FF)[2:])
-        TMP_KEY = list(bin(self.key)[2:])
-        for i in range(0, len(P10_LST)):
-            P10_LST[i] = TMP_KEY[self.P10_T[i]-1]
-        TMP_P10 = "".join(P10_LST)
+        TMP_P10 = self.permutate(self.key, self.P10_T)
         L_P10 = self.circularLeftShift(TMP_P10[0:5])
+        print("L_P10:", bin(L_P10)[2:].zfill(5))
         R_P10 = self.circularLeftShift(TMP_P10[5:])
+        print("R_P10:", bin(R_P10)[2:].zfill(5))
+
         # combine right and left
         P10 = (0x3FF & (L_P10 << 5) )| R_P10
-        print("P10: ", P10)
+
         # form K1
-        P8_LST = ""
-        print("P8_LST: ", P8_LST)
-        P10_LST = bin(P10)[2:].zfill(10)
-        print("P10_LST: ", P10_LST)
+        #P10_LST = bin(P10)[2:].zfill(10)
+        #print("P10_LST: ", P10_LST)
 
         # Permutate P10_LST with P8 to derive the first key 
-        for i in range(0, len(self.P8_T)): 
-            P8_LST += P10_LST[(self.P8_T[i]-1)]
-        print("First Key: ", P8_LST)
+        self.K1 = self.permutate(P10, self.P8_T)
+        print("First Key: ", self.K1)
 
+        L3_P10 = self.circularLeftShift(bin(self.circularLeftShift(bin(L_P10)[2:].zfill(5)))[2:].zfill(5))
+        print("L3_P10: ", bin(L3_P10)[2:].zfill(5))
+        R3_P10 = self.circularLeftShift(bin(self.circularLeftShift(bin(R_P10)[2:].zfill(5)))[2:].zfill(5))
+        print("R3_P10: ", bin(R3_P10)[2:].zfill(5))
 
+        # form k2
+        P8 = (0x3FF & (L3_P10 << 5) )| R3_P10
+        #P8_LST = bin(P8)[2:].zfill(10)
+
+        # Permutate to derive the second key 
+        self.K2 = self.permutate(P8, self.P8_T)
+        print("Second Key: ", self.K2)
+
+    def encrypt(self):
+        permuted_input = self.permutate(self.plaintext, self.IP, 8)
+        
 
 # main
 key = 0x282                             # 10 bit key
-plaintext = 0xAF                        #  8 bit plaintext
+plaintext = 0xF3                        #  8 bit plaintext
 
 test = sDES(key, plaintext)
 print(hex(test.key))
-test.deriveKeys()
+test.deriveKeys() 
+test.encrypt()
