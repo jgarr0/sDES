a1cb3741d65489da537d9970f537e23656c78df8
Joseph Garro <joe_garro@att.net>
2022-08-03 09:58:14 -0400

    Reduce number of tests

--- test driver dictionary.py
+++ test driver dictionary.py
@@ -5,8 +5,8 @@ from pydoc import plain
 import errortesting_from_dict as ert
 
 # values to test
-plaintexts = ["z", "g", "i", "hi", "dr", "ko", "cat", "sav", "joe", "bomb", "blue", "tran"]
-keys = [384, 386, 971, 797, 639, 457, 424, 673, 495 , 987, 795, 931]
+plaintexts = ["z", "hi", "cat", "bomb"]
+keys = [384, 797, 424, 987]
 # keep IV constant as IV does not have to be secret
 IV = 0x96
 
@@ -15,10 +15,15 @@ def main():
     for i in range(0, len(plaintexts)):
         results = []
         f.write("{} takes:\n".format(plaintexts[i]))
+        # j = number of errors
         for j in range (1, 9):
             results.append(ert.adderr(int(keys[i]), plaintexts[i], IV, j))
             f.write("\t{}\n".format(results[j-1]))
             f.flush()
+            if(i == 2 and j == 6):
+                break
+            if(i == 3 and j == 5):
+                break
     f.close()
 
 if __name__ == '__main__':
