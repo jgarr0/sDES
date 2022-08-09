3acfbef5e4a94cfcbbc037b398495e78323bef66
Savannah Rimmele <rimmeles@gmail.com>
2022-08-09 11:57:59 -0400

    Track Errors 0-8

--- test driver brute.py
+++ test driver brute.py
@@ -5,8 +5,8 @@ from pydoc import plain
 import errortesting as ert
 
 # values to test
-plaintexts = ["z", "hi"]#, "cat", "bomb"]
-keys = [384, 797]#, 424, 987]
+plaintexts = ["z", "hi"]# "cat", "bomb"]
+keys = [384, 797] #424, 987]
 # keep IV constant as IV does not have to be secret
 IV = 0x96
 
@@ -15,9 +15,11 @@ def main():
     for i in range(0, len(plaintexts)):
         results = []
         f.write("{} takes:\n".format(plaintexts[i]))
-        results.append(ert.adderr(int(keys[i]), plaintexts[i], IV, j))
-        f.write("\t{}\n".format(results[j-1]))
-        f.flush()
+        # j = number of errors
+        for j in range (0, 9):
+            results.append(ert.adderr(int(keys[i]), plaintexts[i], IV, j))
+            f.write("\t{}\n".format(results[j-1]))
+            f.flush()
     f.close()
 
 if __name__ == '__main__':
